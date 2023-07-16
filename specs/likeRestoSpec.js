5779a08ccd7c1f88138b30fbb60d0e3a2d3a4d8e
rhyman18 <rhyman18@gmail.com>
2023-07-16 22:00:11 +0700

    feat: add test clicked like resto

--- specs/likeRestoSpec.js
+++ specs/likeRestoSpec.js
@@ -1,4 +1,6 @@
+import { async } from 'regenerator-runtime';
 import favoriteInitiator from '../src/scripts/utils/favoriteInitiator';
+import FavoriteResto from '../src/scripts/data/favoriteResto';
 
 describe('Liking A Resto', () => {
   it('should show the add to favorite button when the resto has not been liked before', async () => {
@@ -25,5 +27,23 @@ describe('Liking A Resto', () => {
     });
 
     expect(document.querySelector('[aria-label="unlike this resto"]')).toBeFalsy();
+  });
+
+  it('should be able to like the movie', async () => {
+    document.body.innerHTML = '<div id="fav"></div>';
+
+    await favoriteInitiator.init({
+      favoriteContainer: document.querySelector('#fav'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('#likeButton').dispatchEvent(new Event('click'));
+
+    const resto = await FavoriteResto.getResto(1);
+    expect(resto).toEqual({id: 1});
+
+    FavoriteResto.deleteResto(1);
   });
 });
