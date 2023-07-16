b576f16985afd1d5051c900d314bb060569bb8f3
rhyman18 <rhyman18@gmail.com>
2023-07-16 21:53:07 +0700

    feat: add test unlike resto

--- specs/likeRestoSpec.js
+++ specs/likeRestoSpec.js
@@ -13,4 +13,17 @@ describe('Liking A Resto', () => {
 
     expect(document.querySelector('[aria-label="like this resto"]')).toBeTruthy();
   });
+
+  it('should not show the unlike button when the resto has not been liked before', async () => {
+    document.body.innerHTML = '<div id="fav"></div>';
+
+    await favoriteInitiator.init({
+      favoriteContainer: document.querySelector('#fav'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this resto"]')).toBeFalsy();
+  });
 });
